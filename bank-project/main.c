8d99d3e9336cae6559e7432a4d9bf544b4976280
MonsieurSingh <teghjyot@icloud.com>
2024-10-17 08:33:07 +0200

    Initial Commit

---
+++ bank-project/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  bank-project
+//
+//  Created by Teghjyot Singh on 17/10/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
