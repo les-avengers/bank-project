36d91559bd4f2fa9338c4528a868f67c9841b3f3
MonsieurSingh <teghjyot@icloud.com>
2024-10-17 09:27:29 +0200

    tests: bank-test

---
+++ bank-tests/bank_tests.m
@@ -0,0 +1,36 @@
+//
+//  bank_tests.m
+//  bank-tests
+//
+//  Created by Teghjyot Singh on 17/10/2024.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface bank_tests : XCTestCase
+
+@end
+
+@implementation bank_tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
