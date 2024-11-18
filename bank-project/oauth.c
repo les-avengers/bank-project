2adf9ee56cf2141138d9c0aa0dd3ca590e52b90a
MonsieurSingh <teghjyot@icloud.com>
2024-11-18 20:20:28 +0100

    works but a bit ugly?

---
+++ bank-project/oauth.c
@@ -0,0 +1,8 @@
+//
+//  oauth.c
+//  bank-project
+//
+//  Created by Teghjyot Singh on 17/11/2024.
+//
+
+#include "oauth.h"
---
+++ bank-project/oauth.h
@@ -0,0 +1,13 @@
+//
+//  oauth.h
+//  bank-project
+//
+//  Created by Teghjyot Singh on 17/11/2024.
+//
+
+#ifndef oauth_h
+#define oauth_h
+
+#include <stdio.h>
+
+#endif /* oauth_h */
